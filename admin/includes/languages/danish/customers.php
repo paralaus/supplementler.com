aa290356830b3da120d6bcef60d36ee54b15cb27
Bülent Çetin <paralaus@gmail.com>
2012-10-30 12:54:46 +0200

    dil dosyalari
    dil dosyalari

---
+++ admin/includes/languages/danish/customers.php
@@ -0,0 +1,33 @@
+<?php
+/*
+  $Id: customers.php,v 1.12 2002/01/12 18:46:27 hpdl Exp $
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2002 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+define('HEADING_TITLE', 'Kunder');
+define('HEADING_TITLE_SEARCH', 'Søg:');
+
+define('TABLE_HEADING_FIRSTNAME', 'Fornavn');
+define('TABLE_HEADING_LASTNAME', 'Efternavn');
+define('TABLE_HEADING_ACCOUNT_CREATED', 'Dato oprettet');
+define('TABLE_HEADING_ACTION', 'Mulighed');
+
+define('TEXT_DATE_ACCOUNT_CREATED', 'Dato oprettet:');
+define('TEXT_DATE_ACCOUNT_LAST_MODIFIED', 'Sidst ændret:');
+define('TEXT_INFO_DATE_LAST_LOGON', 'Sidste login:');
+define('TEXT_INFO_NUMBER_OF_LOGONS', 'Antal login:');
+define('TEXT_INFO_COUNTRY', 'Land:');
+define('TEXT_INFO_NUMBER_OF_REVIEWS', 'Antal anmeldelser:');
+define('TEXT_DELETE_INTRO', 'Sikker på du vil slette denne kunde?');
+define('TEXT_DELETE_REVIEWS', 'Slet %s Anmeldelser(s)');
+define('TEXT_INFO_HEADING_DELETE_CUSTOMER', 'Slet kunde');
+define('TYPE_BELOW', 'Skriv');
+define('PLEASE_SELECT', 'Vælg en');
+define('EXISTING_GROUPS', 'Eksisterende prisgrupper:');
+?>
---
+++ admin/includes/languages/turkish/customers.php
@@ -0,0 +1,32 @@
+<?php
+/*
+  $Id: customers.php,v 1.12 2002/01/12 18:46:27 hpdl Exp $
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2002 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+define('HEADING_TITLE', 'Müþteriler');
+define('HEADING_TITLE_SEARCH', 'Ara:');
+
+define('TABLE_HEADING_FIRSTNAME', 'Ýsim');
+define('TABLE_HEADING_LASTNAME', 'Soyisim');
+define('TABLE_HEADING_ACCOUNT_CREATED', 'Üyelik Tarihi');
+define('TABLE_HEADING_ACTION', 'Hareket');
+
+define('TEXT_DATE_ACCOUNT_CREATED', 'Üyelik Tarihi:');
+define('TEXT_DATE_ACCOUNT_LAST_MODIFIED', 'Son Güncelleme:');
+define('TEXT_INFO_DATE_LAST_LOGON', 'Son Giriþ:');
+define('TEXT_INFO_NUMBER_OF_LOGONS', 'Giriþ Sayýsý:');
+define('TEXT_INFO_COUNTRY', 'Ülke:');
+define('TEXT_INFO_NUMBER_OF_REVIEWS', 'Yorum Sayýsý:');
+define('TEXT_DELETE_INTRO', 'Bu müþteriyi silmek istediðinize emin misiniz?');
+define('TEXT_DELETE_REVIEWS', ' %s yorum(lar)ý Sil');
+define('TEXT_INFO_HEADING_DELETE_CUSTOMER', 'Müþteriyi Sil');
+define('TYPE_BELOW', 'Aþaðýya Yaz');
+define('PLEASE_SELECT', 'Birisini Seç');
+?>
